b26c7572c96e41fa45fd116dcfed81533e6f45cd
Juan <juaoose@users.noreply.github.com>
2025-03-07 17:11:06 -0500

    chore: extract suggestion

---
+++ lua/ninetyfive/suggestion.lua
@@ -0,0 +1,96 @@
+local suggestion = {}
+local ninetyfive_ns = vim.api.nvim_create_namespace("ninetyfive_ghost_ns")
+
+suggestion.show = function(bufnr, line, col, message) 
+  -- Clear any existing extmarks in the buffer
+  vim.api.nvim_buf_clear_namespace(bufnr, ninetyfive_ns, 0, -1)
+
+  local virt_lines = {}
+  for _, l in ipairs(vim.fn.split(message, '\n')) do
+      table.insert(virt_lines, { { l, type(c) == 'table' and c.hl_group or 'Comment' } })
+  end
+  local first_line = table.remove(virt_lines, 1)
+
+  -- Set the ghost text using an extmark
+  -- https://neovim.io/doc/user/api.html#nvim_buf_set_extmark()
+  completion_id = vim.api.nvim_buf_set_extmark(bufnr, ninetyfive_ns, line, col, {
+      right_gravity = true,
+      virt_text = first_line,
+      virt_text_pos = "inline",
+      virt_lines = virt_lines,
+      hl_mode = "combine",
+      ephemeral = false,
+  })
+end
+
+suggestion.accept = function()
+  if not completion_id then return end
+
+  local bufnr = vim.api.nvim_get_current_buf()
+  -- Retrieve the extmark and get the suggestion from it
+  local extmark = vim.api.nvim_buf_get_extmark_by_id(bufnr, ninetyfive_ns, completion_id, { details = true })
+
+  if extmark and #extmark > 0 then
+    local line, col = extmark[1], extmark[2]
+    local details = extmark[3]
+    
+    local extmark_text = ""
+    
+    if details.virt_text then
+      for _, part in ipairs(details.virt_text) do
+        extmark_text = extmark_text .. part[1]
+      end
+    end
+
+    print("accepted completion", extmark_text)
+    
+    -- Add the rest of the lines from virt_lines
+    if details.virt_lines then
+      for _, virt_line in ipairs(details.virt_lines) do
+        extmark_text = extmark_text .. "\n"
+        for _, part in ipairs(virt_line) do
+          extmark_text = extmark_text .. part[1]
+        end
+      end
+    end
+    
+    -- Remove the suggestion
+    vim.api.nvim_buf_del_extmark(bufnr, ninetyfive_ns, completion_id)
+
+    -- Inserting the completion has to be done line by line
+    if string.find(extmark_text, "\n") then
+      -- Split the ghost text by newlines
+      local lines = {}
+      for s in string.gmatch(extmark_text, "[^\n]+") do
+        table.insert(lines, s)
+      end
+      
+      -- Insert the first line at the cursor position
+      if #lines > 0 then
+        vim.api.nvim_buf_set_text(bufnr, line, col, line, col, { lines[1] })
+      end
+      
+      -- Insert the rest of the lines as new lines
+      if #lines > 1 then
+        local new_lines = {}
+        for i = 2, #lines do
+          table.insert(new_lines, lines[i])
+        end
+        vim.api.nvim_buf_set_lines(bufnr, line + 1, line + 1, false, new_lines)
+      end
+    else
+      -- No newlines, just insert the text
+      vim.api.nvim_buf_set_text(bufnr, line, col, line, col, { extmark_text })
+    end
+
+    completion_id = nil
+    completion = ""
+    request_id = ""
+  end
+end
+
+suggestion.clear = function(bufnr)
+  vim.api.nvim_buf_clear_namespace(bufnr, ninetyfive_ns, 0, -1)
+end
+
+return suggestion
--- lua/ninetyfive/websocket.lua
+++ lua/ninetyfive/websocket.lua
@@ -1,5 +1,6 @@
 local log = require("ninetyfive.util.log")
 local Queue = require("ninetyfive.queue")
+local suggestion = require("ninetyfive.suggestion")
 
 local Websocket = {}
 
@@ -7,101 +8,7 @@ local Websocket = {}
 local completion_id = ""
 local completion = ""
 local request_id = ""
-local ninetyfive_ns = vim.api.nvim_create_namespace("ninetyfive_ghost_ns")
 local completion_queue = Queue.New() 
-
--- Function to set ghost text in the buffer
-local function set_ghost_text(bufnr, line, col, message)
-    -- Clear any existing extmarks in the buffer
-    vim.api.nvim_buf_clear_namespace(bufnr, ninetyfive_ns, 0, -1)
-
-    local virt_lines = {}
-    for _, l in ipairs(vim.fn.split(message, '\n')) do
-        table.insert(virt_lines, { { l, type(c) == 'table' and c.hl_group or 'Comment' } })
-    end
-    local first_line = table.remove(virt_lines, 1)
-
-    -- Set the ghost text using an extmark
-    -- https://neovim.io/doc/user/api.html#nvim_buf_set_extmark()
-    completion_id = vim.api.nvim_buf_set_extmark(bufnr, ninetyfive_ns, line, col, {
-        right_gravity = true,
-        virt_text = first_line,
-        virt_text_pos = "inline",
-        virt_lines = virt_lines,
-        hl_mode = "combine",
-        ephemeral = false,
-    })
-end
-
-local function clear_suggestion(bufnr)
-    vim.api.nvim_buf_clear_namespace(bufnr, ninetyfive_ns, 0, -1)
-end
-
-local function accept_completion()
-    if not completion_id then return end
-  
-    local bufnr = vim.api.nvim_get_current_buf()
-    -- Retrieve the extmark and get the suggestion from it
-    local extmark = vim.api.nvim_buf_get_extmark_by_id(bufnr, ninetyfive_ns, completion_id, { details = true })
-  
-    if extmark and #extmark > 0 then
-      local line, col = extmark[1], extmark[2]
-      local details = extmark[3]
-      
-      local extmark_text = ""
-      
-      if details.virt_text then
-        for _, part in ipairs(details.virt_text) do
-          extmark_text = extmark_text .. part[1]
-        end
-      end
-
-      print("accepted completion", extmark_text)
-      
-      -- Add the rest of the lines from virt_lines
-      if details.virt_lines then
-        for _, virt_line in ipairs(details.virt_lines) do
-          extmark_text = extmark_text .. "\n"
-          for _, part in ipairs(virt_line) do
-            extmark_text = extmark_text .. part[1]
-          end
-        end
-      end
-      
-      -- Remove the suggestion
-      vim.api.nvim_buf_del_extmark(bufnr, ninetyfive_ns, completion_id)
-
-      -- Inserting the completion has to be done line by line
-      if string.find(extmark_text, "\n") then
-        -- Split the ghost text by newlines
-        local lines = {}
-        for s in string.gmatch(extmark_text, "[^\n]+") do
-          table.insert(lines, s)
-        end
-        
-        -- Insert the first line at the cursor position
-        if #lines > 0 then
-          vim.api.nvim_buf_set_text(bufnr, line, col, line, col, { lines[1] })
-        end
-        
-        -- Insert the rest of the lines as new lines
-        if #lines > 1 then
-          local new_lines = {}
-          for i = 2, #lines do
-            table.insert(new_lines, lines[i])
-          end
-          vim.api.nvim_buf_set_lines(bufnr, line + 1, line + 1, false, new_lines)
-        end
-      else
-        -- No newlines, just insert the text
-        vim.api.nvim_buf_set_text(bufnr, line, col, line, col, { extmark_text })
-      end
-
-      completion_id = nil
-      completion = ""
-      request_id = ""
-    end
-end
 
 -- Function to send a message to the websocket
 function Websocket.send_message(message)
@@ -198,7 +105,7 @@ local function request_completion(args)
     end
 
     -- Also clear any suggestions that were showing
-    clear_suggestion(args.buf)
+    suggestion.clear(args.buf)
 end
 
 -- Function to set up autocommands related to websocket functionality
@@ -209,7 +116,7 @@ function Websocket.setup_autocommands()
 
     -- Make accept_completion available globally
     _G.accept_ninetyfive_completion = function()
-        accept_completion()
+        suggestion.accept()
         return ""  -- This is important for expr mappings to not insert anything
     end
     
@@ -344,7 +251,7 @@ function Websocket.setup_connection(server_uri)
                                         local line = cursor[1] - 1
                                         local col = cursor[2] - 1 -- Is this -1 correct
 
-                                        set_ghost_text(bufnr, line, col, completion)
+                                        suggestion.show(bufnr, line, col, completion)
                                     end
                                 end
 
