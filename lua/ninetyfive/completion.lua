214815e99abd352d6a554289ff07cbce1fbd75f1
Juan <juaoose@users.noreply.github.com>
2025-04-14 14:36:51 -0500

    chore: consolidate req/response/state

---
+++ lua/ninetyfive/completion.lua
@@ -0,0 +1,51 @@
+local log = require("ninetyfive.util.log")
+
+local Completion = {}
+Completion.__index = Completion
+
+-- Create a new Completion instance
+function Completion.new(request_id)
+    local self = setmetatable({}, Completion)
+    self.request_id = request_id
+    self.completion = ""
+    self.consumed = 0
+    self.is_closed = false
+    self.edits = {}
+    self.edit_description = ""
+    self.edit_index = 0
+
+    self.isJumpEdit = false
+    self.isInlineEdit = false
+    self.isJumpHint = false
+    self.isActive = false
+    self.hint = ""
+    self.buffer = nil
+    self.offset = 0
+    self.jumpDestLine = 0
+    self.jumpDestCol = 0
+    return self
+end
+
+function Completion:consume(n)
+    if n > #self.completion then
+        log.debug("edit_state", "Illegal state: trying to mark flush beyond completion length")
+        return
+    end
+    self.consumed = n
+end
+
+function Completion:close()
+    self.is_closed = true
+    if vim.trim(self.completion) == "" then
+        -- Handle a single newline as an edge case, we ignore the completion and directly go to edits
+        self.consumed = #self.completion
+    end
+end
+
+function Completion:next_edit()
+    if self.is_closed and self.consumed == #self.completion and self.editIndex < #self.edits then
+        return self.edits[self.editIndex + 1]
+    end
+end
+
+return Completion
