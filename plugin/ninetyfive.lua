c85b04817c47c452dbfea8fc178416c7de6c9498
Juan R <994594+juaoose@users.noreply.github.com>
2025-05-14 17:25:01 -0500

    chore: purchase link

--- plugin/ninetyfive.lua
+++ plugin/ninetyfive.lua
@@ -1,3 +1,6 @@
+local uv = vim.uv or vim.loop
+local os_uname = uv.os_uname().sysname
+
 -- You can use this loaded variable to enable conditional parts of your plugin.
 if _G.NinetyfiveLoaded then
     return
@@ -34,4 +37,31 @@ else
             vim.notify("API key not set (empty input)", vim.log.levels.WARN)
         end
     end, { desc = "Sets the API Key." })
+
+    vim.api.nvim_create_user_command("NinetyfivePurchase", function()
+        local url = "https://ninetyfive.gg/api/payment"
+        local cmd
+        if os_uname == "Linux" then
+            local candidates = { "xdg-open", "gvfs-open", "gnome-open", "wslview" }
+            for _, candidate_cmd in ipairs(candidates) do
+                if vim.fn.executable(candidate_cmd) == 1 then
+                    cmd = candidate_cmd
+                    break
+                end
+            end
+        elseif vim.loop.os_uname().sysname == "Darwin" then
+            cmd = "open"
+        elseif vim.loop.os_uname().sysname == "Windows" then
+            cmd = "start"
+        end
+
+        if cmd then
+            vim.fn.jobstart({ cmd, url }, { detach = true })
+        else
+            vim.notify(
+                "Make sure one of these tools is installed xdg-open, gvfs-open, gnome-open, wslview, open (MacOS) or start (Windows)",
+                vim.log.levels.WARN
+            )
+        end
+    end, { desc = "Open the purchase site on your browser" })
 end
